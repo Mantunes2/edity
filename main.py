7898fdd06e6e4e523c97ee1ca9b1801a6a359ea7
Matheus Antunes <38018748+Mantunes2@users.noreply.github.com>
2022-01-24 22:37:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ import PySimpleGUI as sg
 
 
 def home():
-  sg.theme('darkgreen2')
   layout = [
-    [sg.Text('Editor')],
-    [sg.Multiline(key='texto', size=(60, 27))],
+    [sg.Text('Editor de Texto')],
+    [sg.Multiline(key='texto', size=(280, 180))],
     [sg.Input(key='file_name'), sg.Button('Salvar'), sg.Button('Cancelar')]
   ]
-  window = sg.Window('EDITOR DE TEXTO', layout, resizable=True)
+  window = sg.Window('EDITOR DE TEXTO', layout, size=(1920, 1080))
   while True:
     event, values = window.read()
     if event == sg.WINDOW_CLOSED:
